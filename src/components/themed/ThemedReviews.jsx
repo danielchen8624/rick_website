ae443a60eee009b2bdbeecba0ae8934653fc9e54
Daniel Chen <daniel.chen0113@gmail.com>
2026-05-01 23:39:52 -0400

    Changes

--- src/components/themed/ThemedReviews.jsx
+++ src/components/themed/ThemedReviews.jsx
@@ -8,6 +8,12 @@ const INITIAL_REVIEWS = [
     text: 'I would like to provide a reference of experience working with Ricardo from E&E Total Contracting and his team. Ricardo from E&E Total Contracting and his team are extremely responsive, efficient and kind. They have accepted and completed in a timely manner and to their best ability every time. I would highly recommend their services to anyone and everyone that requires them. I will myself be using their service now and into the future as I am completely satisfied and impressed with the level of care provided.',
     date: 'March 25, 2026',
   },
+  {
+    name: 'Debbie Anderson, RCM, B.A. (Hons)',
+    company: 'Condominium Manager',
+    text: 'My Superintendent was away for 7 weeks over the Christmas holidays. I had the pleasure of coverage being provided by Edwin Lara of E&E Total Contracting Ltd. He was excellent! His employer, Ricardo, was wonderful to work with as well and all was very seamless while the Super was away during a busy holiday season. If you need coverage, I’m sure Ricardo will do his best to help you out. If you are lucky, you will get Edwin!',
+    date: 'April 28, 2026',
+  },
 ];
 
 const ThemedReviews = () => {
